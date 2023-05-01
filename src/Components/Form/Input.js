0a16d68c97dd35093c901375a83248f86f585bee
guilherme <guilhermekeslley123@gmail.com>
2023-05-01 11:49:28 -0300

    Componentes reutilizaveis

---
+++ src/Components/Form/Input.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Error from '../Helper/Error';
+
+const Input = ({ label, type, name, onChange, onBlur, error, value }) => {
+  return (
+    <>
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+      />
+      <Error>{error}</Error>
+    </>
+  );
+};
+
+export default Input;
