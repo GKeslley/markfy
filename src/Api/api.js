a955c698cc64a978848961a3568a6863fd01e479
guilherme <guilhermekeslley123@gmail.com>
2023-05-02 18:54:11 -0300

    Outros endpoints da api

--- src/Api/api.js
+++ src/Api/api.js
@@ -12,3 +12,40 @@ export const POST_USER = (body) => {
     },
   };
 };
+
+export const LOGIN_USER_POST = (body) => {
+  return {
+    url: API_URL + '/jwt-auth/v1/token',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+};
+
+export const VALIDATE_TOKEN = (token) => {
+  return {
+    url: API_URL + '/jwt-auth/v1/token/validate',
+    options: {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    },
+  };
+};
+
+export const USER_GET = (token) => {
+  return {
+    url: API_URL + '/api/usuario',
+    options: {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    },
+  };
+};
