a6f8ff17fed4ed00b0498afce046da0b86bd6ed6
guilherme <guilhermekeslley123@gmail.com>
2023-05-06 20:11:06 -0300

    Adicionando componente de postar produto

--- src/Components/User/Conta.js
+++ src/Components/User/Conta.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Routes, Route, NavLink } from 'react-router-dom';
 import Aside from '../Aside';
-import ProductForm from './ProductForm';
+import ProductForm from './PostProducts/ProductForm';
 import Settings from './Settings';
 import styles from '../../Css/User/Conta.module.css';
+import ProductCategory from './PostProducts/CategoryPage/ProductCategory';
 
 const Conta = () => {
+  const [category, setCategory] = React.useState(null);
+  const [getSubcategory, setGetSubcategory] = React.useState(null);
+
   return (
     <section className={styles.contaContent}>
       <Aside>
         <ul className="container">
           <li>
-            <NavLink to="produto">Adicionar produto</NavLink>
+            <NavLink to="produto/categoria">Adicionar produto</NavLink>
           </li>
           <li>
             <NavLink to="configuracoes">Configurações</NavLink>
@@ -20,7 +24,20 @@ const Conta = () => {
       </Aside>
 
       <Routes>
-        <Route path="produto" element={<ProductForm />}></Route>
+        <Route
+          path="produto/categoria"
+          element={
+            <ProductCategory
+              setCategory={setCategory}
+              category={category}
+              setGetSubcategory={setGetSubcategory}
+            />
+          }
+        ></Route>
+        <Route
+          path="produto/form"
+          element={<ProductForm category={category} getSubcategory={getSubcategory} />}
+        ></Route>
         <Route path="configuracoes" element={<Settings />}></Route>
       </Routes>
     </section>
