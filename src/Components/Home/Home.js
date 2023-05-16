88fd5f839fe079795a1e8df55d84ef239c8dc58a
guilherme <guilhermekeslley123@gmail.com>
2023-05-15 22:17:23 -0300

    Section dos smartphones padrões

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import Products from '../Products/Products';
+import Slide from '../Reusable/Slide';
+import Imagem1 from '../../Assets/Slide/capa1.png';
+import Imagem2 from '../../Assets/Slide/capa2.png';
+import Imagem3 from '../../Assets/Slide/capa3.png';
+import Smartphones from './Smartphones';
 
-const Home = () => {
-  return <Products />;
+const Home = ({ setDataProduct }) => {
+  return (
+    <div>
+      <Slide imgs={[Imagem3, Imagem1, Imagem2, Imagem3, Imagem1]} />
+      <Smartphones setDataProduct={setDataProduct} />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/Components/Home/Smartphones.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import useFetch from '../../Hooks/useFetch';
+import { PRODUCTS_GET_BY_CATEGORY_SUBCATEGORY } from '../../Api/api';
+import styles from '../../Css/Home/Smartphones.module.css';
+import { ReactComponent as BtnCarousel } from '../../Assets/arrowCarousel.svg';
+import { useNavigate } from 'react-router-dom';
+
+const Smartphones = ({ setDataProduct }) => {
+  const { request, data } = useFetch();
+  const carouselRef = React.useRef();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    const getSmartphones = async () => {
+      const { url, options } = PRODUCTS_GET_BY_CATEGORY_SUBCATEGORY(
+        'eletronicos',
+        'celulares_e_telefones',
+        'markfy@gmail.com',
+      );
+      console.log(url);
+      const response = await request(url, options);
+      console.log(response);
+    };
+    getSmartphones();
+  }, [request]);
+
+  const handleLeftClick = () => {
+    carouselRef.current.scrollLeft -= carouselRef.current.offsetWidth + 16;
+  };
+
+  const handleRightClick = () => {
+    const carousel = carouselRef.current;
+    carousel.scrollLeft += carousel.offsetWidth;
+  };
+
+  const selectProduct = (product) => {
+    setDataProduct(product);
+    navigate(`produto/${product.id}`);
+  };
+
+  return (
+    <div className="container">
+      <div className={styles.carouselContent}>
+        <p>GARANTA SEU NOVO SMARTPHONE</p>
+        <ul className={`${styles.smartphones} flexAlign`} ref={carouselRef}>
+          {data &&
+            data.map((product) => (
+              <li
+                key={product.id + toString(Math.random())}
+                onClick={() => selectProduct(product)}
+              >
+                <picture>
+                  <img src={product.fotos[0].src} alt={product.fotos[0].titulo} />
+                </picture>
+                <span>R$ {product.preco}</span>
+                <p>{product.nome.slice(0, 30).trim()}...</p>
+              </li>
+            ))}
+        </ul>
+        <div className={styles.buttons}>
+          <span className={styles.buttonLeft} onClick={handleLeftClick}>
+            <BtnCarousel />
+          </span>
+          <span className={styles.buttonRight} onClick={handleRightClick}>
+            <BtnCarousel />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Smartphones;
