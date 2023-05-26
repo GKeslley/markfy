41b6ab1cf6b86553b505072ca295cbf40fd64aa3
guilherme <guilhermekeslley123@gmail.com>
2023-05-26 16:45:10 -0300

    Alterações

--- src/Components/Form/Input.js
+++ src/Components/Form/Input.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Error from '../Helper/Error';
 
-const Input = ({ label, type, name, onChange, onBlur, error, value }) => {
+const Input = ({ label, type, name, placeholder, onChange, onBlur, error, value }) => {
   return (
     <>
-      <label htmlFor={name}>{label}</label>
+      {!placeholder && <label htmlFor={name}>{label}</label>}
       <input
         type={type}
         name={name}
         id={name}
+        placeholder={placeholder ? placeholder : ''}
         onChange={onChange}
         onBlur={onBlur}
         value={value}
