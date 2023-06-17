274b84e1532ca98b4d977004121d0cd296dc4969
guilherme <guilhermekeslley123@gmail.com>
2023-06-17 16:28:53 -0300

    Hook para desfavoritar produtos

---
+++ src/Hooks/useUnlikeProduct.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import useFetch from './useFetch';
+import { UNLIKE_PRODUCT } from '../Api/api';
+
+const useUnlikeProduct = ({ getFavoriteProducts }) => {
+  const { request, loading, error } = useFetch();
+
+  const unlikeProduct = React.useCallback(
+    async (slug) => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        const { url, options } = UNLIKE_PRODUCT({ slug, token });
+        const { response } = await request(url, options);
+        if (response.ok) {
+          getFavoriteProducts();
+        }
+      }
+    },
+    [request, getFavoriteProducts],
+  );
+
+  return { unlikeProduct, loading, error };
+};
+
+export default useUnlikeProduct;
