f30a4c526e211228cf2b4a10df51cc3d0b167d82
guilherme <guilhermekeslley123@gmail.com>
2023-08-08 14:52:47 -0300

    Mostrar produtos vendidos dos usuarios

--- src/Components/User/Account/ProductsSold.js
+++ src/Components/User/Account/ProductsSold.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import useFetch from '../../../Hooks/useFetch';
+import { PRODUCTS_SOLD_GET } from '../../../Api/api';
+import styles from '../../../Css/User/ProductsForSale.module.css';
+import usePagination from '../../../Hooks/usePagination';
+import Pagination from '../../Reusable/Pagination';
+import ProductsForSaleSkeleton from '../../Skeletons/ProductsForSaleSkeleton';
+import Image from '../../Helper/Image';
+import { Link } from 'react-router-dom';
 
-const ProductsSold = () => {
-  return <div>ProductsSold</div>;
+const ProductsSold = ({ username }) => {
+  const { request, data } = useFetch();
+  const { actualPage, setTotalItems, totalItems, maxPage } = usePagination(20);
+
+  React.useEffect(() => {
+    const getProducts = async () => {
+      const { url, options } = PRODUCTS_SOLD_GET({
+        page: actualPage,
+        total: '20',
+        user: username,
+      });
+      const { response } = await request(url, options);
+
+      const total = response.headers.get('x-total-count');
+      setTotalItems(total);
+    };
+    getProducts();
+  }, [request, username, actualPage, setTotalItems]);
+
+  console.log(data);
+
+  if (!data) return <ProductsForSaleSkeleton />;
+  return (
+    <>
+      <div className={styles.product}>
+        <h1>
+          {totalItems} {totalItems > 1 ? 'Produtos' : 'Produto'}
+        </h1>
+        <ul className={styles['products-content']}>
+          {data.map(({ preco, fotos, slug, categoria }) => (
+            <li key={slug}>
+              <Link to={`/produto/${categoria}/${slug}`}>
+                <picture>
+                  {fotos && <Image alt="Imagem de produto" src={fotos[0].src} />}
+                </picture>
+                <span className={styles.price}>R$ {preco}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Pagination maxPage={maxPage} actualPage={actualPage} />
+      </div>
+    </>
+  );
 };
 
 export default ProductsSold;
