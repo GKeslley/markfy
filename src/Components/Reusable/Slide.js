52179fe56358d3eac14e51af1fc2164c587de325
guilherme <guilhermekeslley123@gmail.com>
2023-08-26 23:04:50 -0300

    Modificando slide

--- src/Components/Reusable/Slide.js
+++ src/Components/Reusable/Slide.js
@@ -1,167 +1,55 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { debounce } from 'lodash';
+import React from 'react';
+import { register } from 'swiper/element/bundle';
 import styles from '../../Css/ReusablesCss/Slide.module.css';
-import Image from '../Helper/Image';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/element/css/navigation';
+import 'swiper/element/css/pagination';
+import 'swiper/element/css/scrollbar';
+import 'swiper/element/css/autoplay';
+import useMedia from '../../Hooks/useMedia';
+import Button from '../Reusable/Button';
+
+register();
 
 const Slide = ({ imgs }) => {
-  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
-  const [slideTransform, setSlideTransform] = useState(100);
-  const [mouseOver, setMouseOver] = useState(false);
-  const [touchMoveEnabled, setTouchMoveEnabled] = useState(false);
-  const [touchCoordinates, setTouchCoordinates] = useState(false);
-
-  const timeoutMoveSlide = useRef();
-  const SlideContentRef = useRef();
-
-  const imagesLength = imgs.length - 2;
-  const maxMove = (currentSlideIndex + 1) * 100;
-  const transformStyle = 'transform .3s ease, -webkit-transform .3s ease';
-
-  const imagesSrc = imgs.map((src, i) => ({ src, id: i }));
-
-  const passSlideFromButton = useCallback((index) => {
-    setCurrentSlideIndex(index);
-  }, []);
-
-  const handleTouchStart = useCallback((event) => {
-    setTouchMoveEnabled(true);
-    const coord = event.touches[0].clientX;
-    setTouchCoordinates({ touchStartX: coord, touchMoveX: null, coord: null });
-  }, []);
-
-  const handleTouchEnd = useCallback(() => {
-    SlideContentRef.current.style.transition = transformStyle;
-    setTouchMoveEnabled(false);
-    if (slideTransform % 100 === 0) return null;
-    if (touchCoordinates.coord === 'right') {
-      setCurrentSlideIndex((prev) => prev + 1);
-    } else if (touchCoordinates.coord === 'left') {
-      setCurrentSlideIndex((prev) => prev - 1);
-    }
-  }, [slideTransform, touchCoordinates]);
-
-  const handleTouchMove = useCallback(
-    (event) => {
-      if (currentSlideIndex === 0 || currentSlideIndex >= imagesLength + 1) {
-        setCurrentSlideIndex(1);
-      }
-
-      const coord = event.touches[0].clientX;
-      SlideContentRef.current.style.transition = 'none 0s ease 0s';
-      const newCoordinates = {
-        touchStartX: touchCoordinates.touchStartX,
-        touchMoveX: coord,
-        coord: touchCoordinates.touchStartX < coord ? 'left' : 'right',
-      };
-      setTouchCoordinates(newCoordinates);
-
-      if (touchMoveEnabled && newCoordinates.coord) {
-        setSlideTransform((prev) => {
-          if (newCoordinates.coord === 'left') return prev - 1;
-          if (newCoordinates.coord === 'right') return prev + 1;
-          return prev;
-        });
-
-        if (slideTransform >= maxMove) {
-          setSlideTransform(maxMove);
-        }
-      }
-    },
-    [
-      slideTransform,
-      touchCoordinates,
-      touchMoveEnabled,
-      maxMove,
-      currentSlideIndex,
-      imagesLength,
-    ],
-  );
-
-  useEffect(() => {
-    const handleSlideTimeout = debounce(() => {
-      setCurrentSlideIndex((prev) => {
-        if (prev < imagesLength + 1) return prev + 1;
-        return 0;
-      });
-    }, 3000);
-
-    if (!mouseOver || touchMoveEnabled) {
-      timeoutMoveSlide.current = setTimeout(handleSlideTimeout, 3000);
-    }
-
-    const transitionInfinite = setTimeout(() => {
-      if (currentSlideIndex === imagesLength + 1) {
-        SlideContentRef.current.style.transition = 'none';
-        setCurrentSlideIndex(1);
-      } else if (currentSlideIndex === 0) {
-        SlideContentRef.current.style.transition = 'none';
-        setCurrentSlideIndex(imagesLength);
-      }
-    }, 300);
-
-    const transitionAll = setTimeout(() => {
-      if (currentSlideIndex !== imagesLength + 1 && currentSlideIndex !== 0) {
-        SlideContentRef.current.style.transition = transformStyle;
-      }
-    }, 100);
-
-    setSlideTransform(currentSlideIndex * 100);
-
-    return () => {
-      clearTimeout(timeoutMoveSlide.current);
-      clearTimeout(transitionInfinite);
-      clearTimeout(transitionAll);
-    };
-  }, [currentSlideIndex, imagesLength, mouseOver, touchMoveEnabled]);
-
+  const matchMobile = useMedia('(max-width: 830px)');
   return (
-    <section
-      className={styles.content}
-      onMouseOver={() => {
-        if (!touchMoveEnabled) setMouseOver(true);
-      }}
-      onMouseLeave={() => setMouseOver(false)}
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={handleTouchEnd}
-    >
-      <div className={styles.points}>
-        {imagesSrc.slice(1, -1).map(({ id }, i) => (
-          <span
-            key={id}
-            onClick={() => passSlideFromButton(i + 1)}
-            onTouchStart={() => passSlideFromButton(i + 1)}
-            className={currentSlideIndex === i + 1 ? 'active' : ''}
-          ></span>
-        ))}
-      </div>
-      <ul
-        className={styles.slideItems}
-        style={{ transform: `translateX(-${slideTransform}%)` }}
-        ref={SlideContentRef}
+    <section className={styles.content}>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={false}
+        autoplay={{
+          delay: 99999,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        loop={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className={styles['slide-elements']}
       >
-        <li data-index="-1">
-          <Image alt="imagem" src={imagesSrc[0].src} className="container" />
-        </li>
-
-        {imagesSrc.slice(1, -1).map(({ id, src }, i) => (
-          <li
-            key={id}
-            data-index={id}
-            className={currentSlideIndex === i + 1 ? 'active' : ''}
-          >
-            <img src={src} alt={src} className="container" />
-          </li>
+        {imgs.map(({ src, alt, mobile, text }) => (
+          <SwiperSlide key={text} className={styles.slide}>
+            <div style={{ width: '100%' }}>
+              <div className={styles['slide-text']}>
+                <p>{text}</p>
+                <Button className="button-outline">Buscar</Button>
+              </div>
+              <img
+                src={!matchMobile ? src : mobile}
+                alt={alt}
+                width="1600"
+                height="300"
+              />
+              ) )
+            </div>
+          </SwiperSlide>
         ))}
-
-        <li data-index={imagesSrc[imagesSrc.length - 1].id}>
-          <Image
-            alt="imagem"
-            src={imagesSrc[imagesSrc.length - 1].src}
-            className="container"
-          />
-        </li>
-      </ul>
+      </Swiper>
     </section>
   );
 };
